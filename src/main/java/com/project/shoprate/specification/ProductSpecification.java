56d8dfc5f75a6058fb5c9d92eeeaee20c6aeafd5
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-20 18:01:09 -0300

    feat(backend, specification): Create criteria specification to find products by ids

---
+++ src/main/java/com/project/shoprate/specification/ProductSpecification.java
@@ -0,0 +1,25 @@
+package com.project.shoprate.specification;
+
+import com.project.shoprate.domain.Product;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.CollectionUtils;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSpecification {
+
+    public static Specification<Product> carriersInIds(List<String> ids) {
+        return (root, query, cb) -> {
+
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (!CollectionUtils.isEmpty(ids)) {
+                predicates.add(root.get("id").in(ids));
+            }
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
