65142ef134b9d652899e8929910321321902c271
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-20 18:03:02 -0300

    feat(backend, utils): Create pool with string constants

---
+++ src/main/java/com/project/shoprate/util/StringPool.java
@@ -0,0 +1,10 @@
+package com.project.shoprate.util;
+
+public class StringPool {
+
+    public static final String REQUIRED_RATE_MESSAGE = "Rate field is required!";
+    public static final String REQUIRED_PRODUCT_TYPE_MESSAGE = "Product type field is required!";
+    public static final String REQUIRED_NAME_MESSAGE = "Name field is required!";
+    public static final String REQUIRED_PRICE_MESSAGE = "Price field is required!";
+    public static final String REQUIRED_PRODUCT_RATE_MESSAGE = "Product rate is required!";
+}
