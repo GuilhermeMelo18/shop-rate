660b395d781c3f25069ef261d6412c8cd503dd56
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-21 19:28:05 -0300

    feat(frontend, model): Put informations from app component in product component

--- shop-rate-frontend/src/app/app.component.ts
+++ shop-rate-frontend/src/app/app.component.ts
@@ -9,54 +9,7 @@ import { ProductService } from './services/product.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  productList: Array<Product>;
-
-  bagProductList: Array<Product>;
-  productPrices: ProductPricesWrapper;
-
-  productQueryParams: ProductQueryParams;
-
-  constructor(private productService: ProductService) {
-    this.bagProductList = new Array();
-    this.productQueryParams = new ProductQueryParams();
-  }
-
-  ngOnInit() {
-    this.getProductList();
-  }
-
-  async getProductList(): Promise<void> {
-    this.productList = await this.productService.findAll().toPromise();
-  }
-  addProductToBag(product: Product): void {
-
-      const foundBagProduct = this.bagProductList.find(bagProduct => bagProduct.id === product.id);
-      if (!foundBagProduct) {
-        this.bagProductList.push(product);
-      }
-  }
-
-  removeProductToBag(product: Product): void {
-    this.bagProductList = this.bagProductList.filter(bagProduct => {
-        return bagProduct.id !== product.id;
-    });
-
-    if (this.bagProductList && this.bagProductList.length === 0) {
-      this.productPrices = undefined;
-    }
-  }
-
-  async calculateTotalProductsPrices(): Promise<void> {
-
-    if (this.bagProductList && this.bagProductList.length) {
-      const productIds = this.bagProductList.map(product => product.id);
-      this.productQueryParams.ids = productIds;
-      this.productPrices = await this.productService.getProductPrices(this.productQueryParams).toPromise();
-    } else {
-      this.productPrices = undefined;
-    }
-  }
-
+  constructor() {}
 }
--- shop-rate-frontend/src/app/app.module.ts
+++ shop-rate-frontend/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
 import { ProductService } from './services/product.service';
+import { ProductComponent } from './components/product/product.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
