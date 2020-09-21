4dd6bf878cbb57e78602dc19df3479c339cfb153
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-21 13:15:43 -0300

    feat (frontend, component) : Add main component with product prices informations

--- shop-rate-frontend/src/app/app.component.ts
+++ shop-rate-frontend/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './model/product';
+import { ProductPricesWrapper } from './model/product-prices-wrapper';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shop-rate-frontend';
+export class AppComponent implements OnInit {
+
+  productList: Array<Product>;
+
+  bagProductList: Array<Product>;
+  productPrices: ProductPricesWrapper;
+
+  constructor(private productService: ProductService) {
+    this.bagProductList = new Array();
+  }
+
+  ngOnInit() {
+    this.getProductList();
+  }
+
+  async getProductList(): Promise<void> {
+    this.productList = await this.productService.findAll().toPromise();
+  }
+  addProductToBag(product: Product): void {
+      this.bagProductList.push(product);
+  }
+
+  removeProductToBag(product: Product): void {
+    this.bagProductList = this.bagProductList.filter(bagProduct => {
+        return bagProduct.id !== product.id;
+    });
+  }
+
+  calculateTotalProductsPrices(): void {
+    const productIds = this.bagProductList.map(product => product.id);
+  }
+
 }
