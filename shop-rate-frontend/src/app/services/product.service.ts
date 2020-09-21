6971907a8fcbed105e08d6887c69d209fb03ac16
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-21 09:21:02 -0300

    feat (frontend, service): Add service to get product calculated prices with rates

--- shop-rate-frontend/src/app/services/product.service.ts
+++ shop-rate-frontend/src/app/services/product.service.ts
@@ -2,15 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './model/product';
+import { ProductPricesWrapper } from './model/product-prices-wrapper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private rootPath = 'product';
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<Array<Product>> {
-    return this.http.get<Array<Product>>('product');
+    return this.http.get<Array<Product>>(`${this.rootPath}`);
+  }
+
+  getProductPrices(): Observable<ProductPricesWrapper> {
+    return this.http.get<ProductPricesWrapper>(`${this.rootPath}/get-product-prices`);
   }
 }
