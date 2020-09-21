10856a6f432201c07ee755999540a8dae18d2a54
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-21 19:08:51 -0300

    feat(frontend, component): Add bag products and total prices information,  add method to acess total products prices

--- shop-rate-frontend/src/app/app.component.ts
+++ shop-rate-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './model/product';
 import { ProductPricesWrapper } from './model/product-prices-wrapper';
+import { ProductQueryParams } from './model/query-params/product-query-params';
 import { ProductService } from './services/product.service';
 
 @Component({
@@ -15,8 +16,11 @@ export class AppComponent implements OnInit {
   bagProductList: Array<Product>;
   productPrices: ProductPricesWrapper;
 
+  productQueryParams: ProductQueryParams;
+
   constructor(private productService: ProductService) {
     this.bagProductList = new Array();
+    this.productQueryParams = new ProductQueryParams();
   }
 
   ngOnInit() {
@@ -27,17 +31,32 @@ export class AppComponent implements OnInit {
     this.productList = await this.productService.findAll().toPromise();
   }
   addProductToBag(product: Product): void {
-      this.bagProductList.push(product);
+
+      const foundBagProduct = this.bagProductList.find(bagProduct => bagProduct.id === product.id);
+      if (!foundBagProduct) {
+        this.bagProductList.push(product);
+      }
   }
 
   removeProductToBag(product: Product): void {
     this.bagProductList = this.bagProductList.filter(bagProduct => {
         return bagProduct.id !== product.id;
     });
+
+    if (this.bagProductList && this.bagProductList.length === 0) {
+      this.productPrices = undefined;
+    }
   }
 
-  calculateTotalProductsPrices(): void {
-    const productIds = this.bagProductList.map(product => product.id);
+  async calculateTotalProductsPrices(): Promise<void> {
+
+    if (this.bagProductList && this.bagProductList.length) {
+      const productIds = this.bagProductList.map(product => product.id);
+      this.productQueryParams.ids = productIds;
+      this.productPrices = await this.productService.getProductPrices(this.productQueryParams).toPromise();
+    } else {
+      this.productPrices = undefined;
+    }
   }
 
 }
