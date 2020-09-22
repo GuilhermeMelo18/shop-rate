c39e8ae586fdc5e82e89a795f689fcf25521af4c
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-21 23:26:21 -0300

    feat(backend, repository): Add find by product type named query

--- shop-rate-backend/src/main/java/com/project/shoprate/repository/ProductRateRepository.java
+++ shop-rate-backend/src/main/java/com/project/shoprate/repository/ProductRateRepository.java
@@ -1,8 +1,13 @@
 package com.project.shoprate.repository;
 
 import com.project.shoprate.domain.ProductRate;
+import com.project.shoprate.domain.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface ProductRateRepository extends JpaRepository<ProductRate, Long>, JpaSpecificationExecutor<ProductRate> {
+
+    public List<ProductRate> findByProductType(ProductType productType);
 }
