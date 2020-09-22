94bb67f7caec0bdf0bb7f62ffc4b73ef3953fd2a
Guilherme <guilherme.silva@hivecloud.com.br>
2020-09-22 14:20:04 -0300

    feat(domain): Add constructors

--- shop-rate-backend/src/main/java/com/project/shoprate/domain/Product.java
+++ shop-rate-backend/src/main/java/com/project/shoprate/domain/Product.java
@@ -1,8 +1,5 @@
 package com.project.shoprate.domain;
 
-import com.project.shoprate.util.StringPool;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,30 +8,22 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Transient;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
 @Entity
-@NoArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = StringPool.REQUIRED_NAME_MESSAGE)
     private String name;
 
-    @NotNull(message = StringPool.REQUIRED_PRICE_MESSAGE)
     private BigDecimal price;
 
     @ManyToMany
-    @NotNull(message = StringPool.REQUIRED_PRODUCT_RATE_MESSAGE)
-    @Valid
     @JoinTable(name = "product_product_rate", joinColumns = {
             @JoinColumn(name = "product_id") }, inverseJoinColumns = { @JoinColumn(name = "product_rate_id") })
     private List<ProductRate> productRateList;
--- shop-rate-backend/src/main/java/com/project/shoprate/domain/ProductRate.java
+++ shop-rate-backend/src/main/java/com/project/shoprate/domain/ProductRate.java
@@ -1,7 +1,5 @@
 package com.project.shoprate.domain;
 
-import com.project.shoprate.util.StringPool;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,7 +7,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
@@ -19,15 +16,19 @@ public class ProductRate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = StringPool.REQUIRED_RATE_MESSAGE)
     private BigDecimal rate;
 
-    @NotNull(message = StringPool.REQUIRED_PRODUCT_TYPE_MESSAGE)
     @Enumerated(EnumType.STRING)
     @Column(unique = true)
     private ProductType productType;
 
     public ProductRate(BigDecimal rate, ProductType productType) {
+        this.rate = rate;
+        this.productType = productType;
+    }
+
+    public ProductRate(Long id, BigDecimal rate, ProductType productType) {
+        this.id = id;
         this.rate = rate;
         this.productType = productType;
     }
